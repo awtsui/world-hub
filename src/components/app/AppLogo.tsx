fa200cd7fd33c9ef7c5f2b1364e178ed4789d80a
Alvin <awtsui@uci.edu>
2024-01-17 02:08:57 -0800

    feat: added new logo

--- src/components/app/AppLogo.tsx
+++ src/components/app/AppLogo.tsx
@@ -1,18 +1,17 @@
 import Image from 'next/image';
+import { AspectRatio } from '../ui/aspect-ratio';
 
 export default function AppLogo() {
   return (
-    <div className="flex items-center">
-      <div className="relative w-8 h-8 mr-2">
+    <div className="w-[190px] h-auto">
+      <AspectRatio ratio={3 / 1}>
         <Image
           alt="logo"
-          className="hidden md:block cursor-pointer"
+          className="cursor-pointer object-cover"
           fill
           src="/logo.png"
         />
-      </div>
-
-      <p className="text-3xl font-bold">WorldHub</p>
+      </AspectRatio>
     </div>
   );
 }
