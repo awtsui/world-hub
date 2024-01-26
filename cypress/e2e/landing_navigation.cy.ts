abde228b25ba6f86f8a99bff269ea679ab0aeddf
Alvin <awtsui@uci.edu>
2024-01-25 20:39:39 -0800

    tweak6

--- cypress/e2e/landing_navigation.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('Landing page', () => {
-  it('redirects successfully to app marketplace', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('[data-testid="explore-button"]').click();
-    cy.location('host').should('contain', 'app');
-  });
-});
